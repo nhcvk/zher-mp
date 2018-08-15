034c4d82ad57d1c4f5c48156a459ecfc3a0fb927
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-15 10:44:39 +0800

    created the show page and detail page, login code, not working yet

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,8 +1,23 @@
 // pages/details/details.js
+const app = getApp()
+const myRequest = require('../../lib/request');
+
 Page({
 
   data: {
-  
+    items: []
   },
+
+  onLoad: function () {
+    let page = this
+    myRequest.get({
+      path: 'places',
+      success(res) {
+        console.log(res)
+        page.setData({ items: res.data.places })
+      }
+    })
+  },
+
 
 })
