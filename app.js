7a2a2d4626569b4c2430647fe0565c28f27c17bf
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-16 12:17:17 +0800

    moved the login procedure from app.js to index + allowed users to move to show only after login procedure is completed

--- app.js
+++ app.js
@@ -2,25 +2,10 @@
 
 
 App({
-  onLaunch: function () {
-    const host = 'http://localhost:3000/api/v1/'
-    console.log('processing to login')
-    wx.login({
-      success: res => {
-        console.log(res)
-        wx.request({
-          url: host + 'login',
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success: res => {
-            console.log(res)
-            this.globalData.userId = res.data.userId
-          }
-        })
-      }
-    })
-  },
-  globalData: {}
+  globalData: {},
+
+  onLaunch: wx.redirectTo({
+    url: '/pages/index/index'
+  })
+  
 })  
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,56 @@ const app = getApp()
 
 Page({
   data: {
+    hasUserID: false,
+    visibility: "visibility: hidden"
   },
 
-  goShow: function (e) {
-    wx.navigateTo({
-      url: '/pages/show/show'
+  onLoad: function () {
+
+    console.log(111, this.data.hasUserID)
+    const host = 'http://localhost:3000/api/v1/'
+    console.log('processing to login')
+    wx.login({
+      success: res => {
+        console.log(res)
+        wx.request({
+          url: host + 'login',
+          method: 'POST',
+          data: {
+            code: res.code
+          },
+          success: res => {
+            console.log(res)
+            this.globalData.userId = res.data.userId
+            console.log(888, this.data.hasUserID)
+            this.setData ({
+              hasUserID: true
+            })
+            console.log(999, this.data.hasUserID)
+          },
+        })
+        let page = this
+        console.log(page.data.currentPage)
+        page.setData ({
+          visibility: "visibility:show"
+        })
+      }
     })
+  },
+
+  globalData: {},
+
+  goShow: function (e) {
+    if (this.data.hasUserID = true) {
+      console.log(789, this.hasUserID)
+      wx.navigateTo({
+        url: '/pages/show/show'
+      })
+    } else {
+      wx.showToast({
+        title: 'Loading data',
+      })
+    }
   },
 
 })
