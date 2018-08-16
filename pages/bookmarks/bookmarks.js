6dbdcc24e2401c9aaaeb48638975782b91df3f56
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-16 16:18:32 +0800

    started working on the bookmarks js

--- pages/bookmarks/bookmarks.js
+++ pages/bookmarks/bookmarks.js
@@ -1,9 +1,24 @@
 // pages/bookmarks/bookmarks.js
+const app = getApp()
+const myRequest = require('../../lib/request');
+
 Page({
 
-
   data: {
-  
+    items: []
+    user
   },
 
+  onLoad: function () {
+    let page = this
+    myRequest.get({
+      path: '${}',
+      success(res) {
+        console.log(res)
+        page.setData({
+          items: res.data.places
+        })
+      }
+    })
+  }
 })
