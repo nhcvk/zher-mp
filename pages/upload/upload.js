3a74fc7437d2b05dae6be83214a802fa7e47857d
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-16 16:04:20 +0800

    implemented leancloud, started working on the upload page

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -1,8 +1,150 @@
 // pages/upload/upload.js
+const app = getApp()
+const myRequest = require('../../lib/request');
+const AV = require('../../utils/av-weapp-min.js');
+
 Page({
 
   data: {
-  
+    showLocation: false,
+    items: [],
+    margin: "width: 0",
+    bookmark: "../../assets/icons/bookmark.png",
+    currentPage: 0
   },
+
+  onShow: function () {
+    var that = this
+    console.log("that >> ")
+    console.log(that)
+    console.log(that.data.is_take_photo)
+    if (!that.data.is_take_photo) {
+      // Mark as already take picture
+      that.data.is_take_photo = true
+
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['compressed'],
+        sourceType: ['camera', 'album'],
+        success: function (res) {
+          var tempFilePath = res.tempFilePaths[0]
+
+          console.log("Temp file path >>")
+          console.log(tempFilePath)
+          that.setData({
+            is_sending: true,
+            imageSrc: tempFilePath
+          })
+          console.log("Have Image >>")
+          console.log(that.data.haveImage)
+          // console.log(that.data.imgSrc)
+
+          // #####LEANCLOUD PART --- SEND IMG
+          console.log("Processing send img to LeanCloud >>")
+          new AV.File('file-name', {
+            blob: {
+              uri: tempFilePath,
+            },
+          }).save().then(
+            file => {
+              console.log("Yeah..This is img url in LeanCloud >>")
+              console.log(that)
+              console.log(file.url())
+              that.setData({
+                is_sending: false,
+                haveImage: true,
+                imageUrl: file.url()
+              })
+            }
+          ).catch(console.error);
+          // ######LEANCLOUD PART --- SEND IMG
+        }
+      })
+    }
+  },
+
+
+  onLoad: function () {
+    let page = this
+    myRequest.get({
+      path: 'places',
+      success(res) {
+        console.log(res)
+        page.setData({
+          items: res.data.places
+        })
+      }
+    })
+  },
+
+  scroll: function (e) {
+    this.setData({
+      filter: `filter: blur(${e.detail.scrollTop / 60}rpx);`
+    })
+  },
+
+  goMenu: function (e) {
+    if (this.data.margin == "width: 0") {
+      this.setData({
+        margin: "width: 250",
+        movable: "margin-left: 250px"
+      })
+    }
+    else {
+      this.setData({
+        margin: "width: 0",
+        movable: "margin-left: 0px"
+      })
+    }
+  },
+
+  pageChange: function (e) {
+    console.log(e)
+    this.setData({
+      filter: "filter: blur(0px);",
+      currentPage: e.detail.current
+    })
+    console.log(this.data.currentPage)
+  },
+
+  bookmark: function (e) {
+    let page = this
+    console.log(page.data.currentPage)
+    myRequest.post({
+      path: `users/1/bookmarks`,
+      data: {
+        user_id: 1,
+        place_id: page.data.currentPage + 1,
+      }
+    }),
+      page.setData({
+        bookmark: "../../assets/icons/bookmarked.png"
+      }),
+      wx.showToast({
+        title: 'SAVED'
+      })
+  },
+
+  pinLocation: function (e) {
+
+    let that = this
+
+    wx.chooseLocation({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          value_location: res.name,
+          value_latitude: res.latitude,
+          value_longitude: res.longitude
+        })
+      },
+      fail: function (e) {
+        console.log(e)
+      },
+      cencel: function (e) {
+        console.log(e)
+      }
+    })
+  }
 
 })
